e1e8b937ad01e2cda84f175d4f9435bad7d1c1cc
Akash-Thakur44 <142517514+Akash-Thakur44@users.noreply.github.com>
2025-01-20 18:36:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+//add hovered class to selected list class
+let list = document.querySelectorAll(".navigation li");
+
+function activeLink(){
+    list.forEach((item)=> {
+        item.classList.remove("hovered");
+    });
+    this.classList.add("hovered");
+}
+
+list.forEach(item => item.addEventListener("mouseover", activeLink));
+
+/*// Menu Toggle
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
+
+toggle.onclick = function () {
+  navigation.classList.toggle("active");
+  main.classList.toggle("active");
+};*/
+
+const sidebar = document.querySelector(".sidebar");
+const sidebarToggler = document.querySelector(".sidebar-toggler");
+const menuToggler = document.querySelector(".menu-toggler");
+// Ensure these heights match the CSS sidebar height values
+let collapsedSidebarHeight = "56px"; // Height in mobile view (collapsed)
+let fullSidebarHeight = "calc(100vh - 32px)"; // Height in larger screen
+// Toggle sidebar's collapsed state
+sidebarToggler.addEventListener("click", () => {
+  sidebar.classList.toggle("collapsed");
+});
+// Update sidebar height and menu toggle text
+const toggleMenu = (isMenuActive) => {
+  sidebar.style.height = isMenuActive ? `${sidebar.scrollHeight}px` : collapsedSidebarHeight;
+  menuToggler.querySelector("span").innerText = isMenuActive ? "close" : "menu";
+}
+// Toggle menu-active class and adjust height
+menuToggler.addEventListener("click", () => {
+  toggleMenu(sidebar.classList.toggle("menu-active"));
+});
+// (Optional code): Adjust sidebar height on window resize
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 1024) {
+    sidebar.style.height = fullSidebarHeight;
+  } else {
+    sidebar.classList.remove("collapsed");
+    sidebar.style.height = "auto";
+    toggleMenu(sidebar.classList.contains("menu-active"));
+  }
+});
